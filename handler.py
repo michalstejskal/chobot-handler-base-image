414c4ca961e8f95e075a48773825b1e7019a189d
Michal Stejskal <mstejskal@newps.cz>
2018-08-15 22:28:02 +0200

    Initial commit

---
+++ handler.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import requests
+import os
+import user_code as user_code
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+	val = user_code.handle_pes()
+	return "HELLO WROLD! vraceno z userCode: "+ val
+
+
+if __name__ == "__main__":
+	print("MOCK API SECOND STARTED")
+	app.run(debug=True, host="0.0.0.0", port=5000)
+
