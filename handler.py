28a3ac48f70cccaef7af8572a12e6d72f43b4fd9
Michal Stejskal <mstejskal@newps.cz>
2018-09-08 18:03:44 +0200

    Revert "verze2"
    This reverts commit 6972f9a3df35f2e0d3e4a1d445d14028ed5b7c65.

--- handler.py
+++ handler.py
@@ -1,3 +1,18 @@
-def handle(data, context):
-    print("prijal VERZE 2 PICOjsem data")
+from flask import Flask
+import requests
+import os
+import user_code as user_code
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+	val = user_code.handle_pes()
+	return "HELLO WROLD! vraceno z userCode: "+ val
+
+
+if __name__ == "__main__":
+	print("MOCK API SECOND STARTED")
+	app.run(debug=True, host="0.0.0.0", port=5000)
 
